4097eea87a19f72eac5b69aeca16a423ead56a47
aaldiiieee <prmdy02@gmail.com>
2025-02-01 12:32:49 +0700

    delete file

--- routes/commissionRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getCommissionByMonth } from "../controllers/commissionController.js";
-
-const router = express.Router();
-
-router.get("/get-commission-by-month", getCommissionByMonth);
-
-export default router;
--- routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { makePayment, getPayments } from '../controllers/paymentsController.js';
-
-const router = express.Router();
-
-router.post('/make-payment', makePayment);
-router.get('/get-payments', getPayments);
-
-export default router;
